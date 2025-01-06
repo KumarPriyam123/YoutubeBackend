bab2aab221e9c28cb880c2f36d4910952406433f
KPriYam-123 <231210062@nitdelhi.ac.in>
2025-01-06 22:17:57 +0530

    Utils Code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import connectDB from "./db/index.js";
 //require("dotenv").config({path: "./.env"});
-
+import app from "./app.js";
 
 import dotenv from "dotenv";
 dotenv.config({
     path: "./.env"
 });
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("Error: ",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+}
+)
+.catch((error)=>{
+    console.log("MongoDb connection failed ERROR: ",error);
+    throw error;
+})
 
 /*
 import express from "express";
