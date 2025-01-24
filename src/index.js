a1b4e01e8500d668ba7643b8ed1fbf693d76be67
KPriYam-123 <231210062@nitdelhi.ac.in>
2025-01-24 19:52:43 +0530

    all setup config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import connectDB from "./db/index.js";
 //require("dotenv").config({path: "./.env"});
-import app from "./app.js";
+import {app} from "./app.js";
 
 import dotenv from "dotenv";
 dotenv.config({
